b6017ac9cc1afb3112960a8a59a0c1e08f285a4e
Alexandre <alexandrengirao@hotmail.com>
2021-03-13 19:52:53 -0300

    lab 3 preparacao

---
+++ Laboratório 2/Laboratório 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratório_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
